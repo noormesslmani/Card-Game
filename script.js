8ee2a6c57df9f6d0f9c41d19ad5f128b0f1470ee
noormesslmani <noor.messlmani@gmail.com>
2022-09-16 18:09:57 +0300

    added some line of code in js to hide the matching cards

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
 const cards =  document.getElementsByClassName('card-inner')
-const memory=[]
+
 const logos=['python','java','c']
 const logoCards={}
+var memory=[]
 
-
-console.log(logoCards)
-for (let i=0;i<6;i++){
-    cards[i].addEventListener('click', ()=>{
-        cards[i].classList.toggle('rotate')
-        memory.push(cards[i])
-        // if(memory.length==2){
-
-        // }
+for (let card of cards){
+    card.addEventListener('click', ()=>{
+        if(memory.length==0){
+            card.classList.toggle('rotate')
+            memory.push(card)
+        }
+        else if (memory.length==1){
+            if(card!=memory[0]){
+                card.classList.toggle('rotate')
+                memory.push(card)
+            }
+        }
+        else {
+            console.log(memory[0].classList)
+            console.log(memory[1].classList)
+            if (memory[0].classList.value==memory[1].classList.value){
+                memory[0].classList.add('hide')
+                memory[1].classList.add('hide')
+            }
+            else{
+                memory[0].classList.toggle('rotate')
+                memory[1].classList.toggle('rotate')
+            }
+            memory=[]
+        } 
     })  
 }
 
